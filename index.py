0c7bccd95a22a16992d6e452d6a506fde3ec0530
Vudtipat Saishana <Vudtipat.Saishana@krungsrinimble.com>
2022-05-15 14:00:16 +0700

    Add flask

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 import schedule
 import time
+from flask import Flask, request, abort
+import json
 
 label = {   
             "HPDS.jpg":"ไฮพรีเมียมดีเซล",
@@ -43,8 +45,47 @@ def get_Oil_price():
 
 schedule.every(5).minutes.do(get_Oil_price)
 
-while(1):
-    schedule.run_pending()
-    if not schedule.jobs:
-        break
-    time.sleep(1)
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def webhook():
+    if request.method == 'POST':
+        payload = request.json
+        Reply_token = payload['events'][0]['replyToken']
+        print(Reply_token)
+        message = payload['events'][0]['message']['text']
+        print(message)
+        if 'ดี' in message :
+            Reply_messasge = 'ดีมาก'
+            ReplyMessage(Reply_token,Reply_messasge,'xzhPj3z8pzT2lEPVN9pKS1oHPNsON9Bn6hm1vqYV6TC5SryjkaW2OD9D5pHsI+OrWOz5hTz970KyeXHzLJYZJ7JUDtf6PkGXRFykLT8QxKHgmBp1RwHhZ4YlatYEsIOqtGm15IWfI6YBcg68+koR6gdB04t89/1O/w1cDnyilFU=') #ใส่ Channel access token
+        return request.json, 200
+    else:
+        abort(400)
+
+def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'Bearer {}'.format(Line_Acees_Token)
+    print(Authorization)
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+    data = {
+        "replyToken":Reply_token,
+        "messages":[{
+            "type":"text",
+            "text":TextMessage
+        }]
+    }
+    data = json.dumps(data)
+    r = requests.post(LINE_API, headers=headers, data=data)
+    return 200
+if __name__ == '__main__':
+    app.run(debug=True)
+    while(1):
+        schedule.run_pending()
+        if not schedule.jobs:
+            break
+        time.sleep(1)
+
+
